7fdf10e2482b11d19acc2bbba304dda5c36663fb
Amirajhajh <amir.j.hormozi805@gmail.com>
2026-08-01 19:20:44 +0330

    Fix wsgi.py: Remove websocket routing to prevent NameError

--- Anamis/messenger_project/wsgi.py
+++ Anamis/messenger_project/wsgi.py
@@ -8,20 +8,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-# import messenger_project.routing
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger_project.settings')
-
-
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            messenger_project.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_wsgi_application()
