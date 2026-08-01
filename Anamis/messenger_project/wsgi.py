cb480f21257460878d6a36737119635e18272991
Amirajhajh <amir.j.hormozi805@gmail.com>
2026-08-01 19:15:34 +0330

    Fix incorrect import in wsgi.py

--- Anamis/messenger_project/wsgi.py
+++ Anamis/messenger_project/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import messenger_project.routing
+# import messenger_project.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger_project.settings')
 
