6e6d74a02bc552d6c0077d5d5cd6b877b7b05f06
Amirajhajh <amir.j.hormozi805@gmail.com>
2026-08-02 20:03:25 +0330

    Update chat logic and routing configurations

--- Anamis/chat/consumers.py
+++ Anamis/chat/consumers.py
@@ -1,20 +1,29 @@
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+
+from .models import Message, Chat
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
 
-        self.room_name = self.scope['url_route']['kwargs']['room_id']
-        self.room_group_name = f"chat_{self.room_name}"
+        self.chat_id = self.scope['url_route']['kwargs']['chat_id']
+
+        self.room_group_name = f"chat_{self.chat_id}"
+
 
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
+
         await self.accept()
+
+
 
     async def disconnect(self, close_code):
 
@@ -23,20 +32,54 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+
+
     async def receive(self, text_data):
 
         data = json.loads(text_data)
 
+        content = data.get("message")
+
+
+        message = await self.save_message(content)
+
+
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'chat_message',
-                'message': data['message']
+                "type": "chat_message",
+                "message": message.content,
+                "time": message.created_at.strftime("%H:%M"),
+                "sender": message.sender.username
             }
         )
 
+
+
     async def chat_message(self, event):
 
-        await self.send(text_data=json.dumps({
-            'message': event['message']
-        }))
+        await self.send(
+            text_data=json.dumps({
+                "message": event["message"],
+                "time": event["time"],
+                "sender": event["sender"]
+            })
+        )
+
+
+
+    @database_sync_to_async
+    def save_message(self, content):
+
+        user = self.scope["user"]
+
+        chat = Chat.objects.get(
+            id=self.chat_id
+        )
+
+
+        return Message.objects.create(
+            sender=user,
+            chat=chat,
+            content=content
+        )
