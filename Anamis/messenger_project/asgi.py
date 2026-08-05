874de6805cac66a327a35d84e9736c0743f5ba5c
Amirajhajh <amir.j.hormozi805@gmail.com>
2026-08-05 22:11:57 +0330

    Fix WebSocket logic and ASGI configuration for Render

--- Anamis/messenger_project/asgi.py
+++ Anamis/messenger_project/asgi.py
@@ -1,20 +1,26 @@
 import os
 
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-from django.core.asgi import get_asgi_application
 import chat.routing
 
+
 os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    'messenger_project.settings'
+    "DJANGO_SETTINGS_MODULE",
+    "messenger_project.settings",
 )
+
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
+
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
     ),
 })
+
