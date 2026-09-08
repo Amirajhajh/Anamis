92e99a55656a9b199c3d94c5ce7108f4d901a350
Amirajhajh <amir.j.hormozi805@gmail.com>
2026-09-08 12:48:13 +0330

    Change registration from phone to email OTP

--- Anamis/messenger_project/settings.py
+++ Anamis/messenger_project/settings.py
@@ -169,8 +169,8 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('AnamisMessager@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('xxmb siaz misv trlz')
 
 DEFAULT_FROM_EMAIL = os.environ.get(
     'DEFAULT_FROM_EMAIL',
