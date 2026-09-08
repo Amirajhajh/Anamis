3bb9101c1a017736c7f0ed3122528c0ec3addd69
Amirajhajh <amir.j.hormozi805@gmail.com>
2026-09-08 13:10:15 +0330

    Change registration from phone to email OTP

--- Anamis/messenger_project/settings.py
+++ Anamis/messenger_project/settings.py
@@ -152,28 +152,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-
-# EMAIL_HOST_USER = 'AnamisMessager@gmail.com'
-# EMAIL_HOST_PASSWORD = 'xxmb siaz misv trlz'
-
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get('AnamisMessager@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('xxmb siaz misv trlz')
+EMAIL_HOST_USER = 'AnamisMessager@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxmb siaz misv trlz'
 
-DEFAULT_FROM_EMAIL = os.environ.get(
-    'DEFAULT_FROM_EMAIL',
-    EMAIL_HOST_USER
-)
-EMAIL_TIMEOUT = 10
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
